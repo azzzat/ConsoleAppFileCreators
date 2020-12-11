3d76aad3b75183108b064fee67af0bacd11a9563
Azzzat <arbuz4az@yandex.ru>
2020-12-11 21:53:07 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppFileCreator/ConsoleAppFileCreator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFileCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"..\..\..\..\Files\1000items.txt";
+            StringBuilder finalStringBuilder = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                String article;
+                if (i < 100)
+                {
+                    article = "Кросовки";
+                }
+                else if (i < 300)
+                {
+                    article = "Футболка";
+                }
+                else if (i < 700)
+                {
+                    article = "Спортивка";
+                }
+                else
+                {
+                    article = "Шапка";
+                }
+
+                int price = rnd.Next(1, 1000);
+
+                finalStringBuilder.Append(i);
+                finalStringBuilder.Append("\t");
+                finalStringBuilder.Append(article);
+                finalStringBuilder.Append("\t");
+                finalStringBuilder.Append(price);
+                finalStringBuilder.Append("\n");
+            }
+
+            
+            try 
+            {
+                File.WriteAllText(path, finalStringBuilder.ToString());
+            } 
+            catch
+            {
+                Console.WriteLine("Возникло исключение при записи файла");
+            }
+
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+            Console.ReadLine();
+        }
+    }
+}
